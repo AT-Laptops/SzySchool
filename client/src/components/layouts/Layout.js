9562e43e521275947eb5b383caa6a3ea7af5561c
Mikołaj Ewald <mikolajewald@gmail.com>
2020-02-11 23:34:00 +0100

    Set up

---
+++ client/src/components/layouts/Layout.js
@@ -0,0 +1,10 @@
+import React, { Fragment} from 'react';
+import Sidebar from './Sidebar.js';
+
+const Layout = () => {
+    return (
+        <Sidebar></Sidebar>
+    )
+}
+
+export default Layout;
---
+++ client/src/components/layouts/Sidebar.js
@@ -0,0 +1,9 @@
+import React, {Fragment} from 'react';
+
+const Sidebar = () => {
+    return (
+        <p>Hello</p>
+    )
+}
+
+export default Sidebar;
