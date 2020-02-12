c8150457e671d0cd06ee980d3438c56b860558c4
Norbert Bujny <norbertbujny@gmail.com>
2020-02-12 22:36:24 +0100

    test

---
+++ client/src/components/containers/Navigation.js
@@ -0,0 +1,25 @@
+import './../../App.css';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navigation = () => {
+
+    const [state, setState] = useState('home');
+
+    return (
+        // <div onClick={() => setState(!state)}  className={ state ? 'navbar__element navbar__element--active' : 'navbar__element'}>
+        //     <Link to="/" className="navbar__element__link"></Link>
+        // </div>
+
+        <nav>
+            <div onClick={() => setState('home')} className={state === 'home' ? 'navbar__element navbar__element--active' : 'navbar__element'}>
+                <Link to="/" className="navbar__element__link" ></Link>
+            </div>
+            <div onClick={() => setState('login')} className={state === 'login' ? 'navbar__element navbar__element--active' : 'navbar__element'}>
+                <Link to="/login" className="navbar__element__link" ></Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navigation;
--- client/src/components/layouts/Sidebar.js
+++ client/src/components/layouts/Sidebar.js
@@ -1,16 +1,20 @@
-import React, {Fragment, useState} from 'react';
+import React from 'react';
 import './../../App.css';
-import SidebarElement from './../containers/SidebarElement';
+// import SidebarElement from './../containers/SidebarElement';
+import Navigation from './../containers/Navigation'
 
 const Sidebar = () => {
 
     return (
-        <nav className="navbar">
-            <SidebarElement></SidebarElement>
-            <SidebarElement></SidebarElement>
-            <SidebarElement></SidebarElement>
-            <SidebarElement></SidebarElement>
-        </nav>
+        // <nav className="navbar">
+        //     <SidebarElement></SidebarElement>
+        //     <SidebarElement></SidebarElement>
+        //     <SidebarElement></SidebarElement>
+        //     <SidebarElement></SidebarElement>
+        // </nav>
+
+        <Navigation>
+        </Navigation>
     );
 }
 
