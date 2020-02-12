5f0a1241532d3bba248051227896b02d271adb8a
Mikołaj Ewald <mikolajewald@gmail.com>
2020-02-12 21:02:07 +0100

    sidebar

--- client/src/components/layouts/Sidebar.js
+++ client/src/components/layouts/Sidebar.js
@@ -1,9 +1,18 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
+import { Link } from 'react-router-dom';
+import './../../App.css';
 
 const Sidebar = () => {
+
+    const [state, setState] = useState(false);
+
     return (
-        <p>Hello</p>
-    )
+        <nav className="navbar">
+            <div className="navbar__element" onClick={() => setState(!state)} style={{ backgroundColor: state ? '#612377' : 'transparent' }}>
+                <Link to="/" className="navbar__element__link"></Link>
+            </div>
+        </nav>
+    );
 }
 
 export default Sidebar;
