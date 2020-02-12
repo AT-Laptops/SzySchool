52728f1ec23b6bf760c82df6912cbc1d46adec39
Mikołaj Ewald <mikolajewald@gmail.com>
2020-02-12 21:36:06 +0100

    SidebarElement Component

--- client/src/components/layouts/Sidebar.js
+++ client/src/components/layouts/Sidebar.js
@@ -1,16 +1,13 @@
 import React, {Fragment, useState} from 'react';
-import { Link } from 'react-router-dom';
 import './../../App.css';
+import SidebarElement from './../containers/SidebarElement';
 
 const Sidebar = () => {
 
-    const [state, setState] = useState(false);
-
     return (
         <nav className="navbar">
-            <div className="navbar__element" onClick={() => setState(!state)} style={{ backgroundColor: state ? '#612377' : 'transparent' }}>
-                <Link to="/" className="navbar__element__link"></Link>
-            </div>
+            <SidebarElement></SidebarElement>
+            <SidebarElement></SidebarElement>
         </nav>
     );
 }
