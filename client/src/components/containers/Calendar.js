8ac7edde17c5272e90907aa108141feaab4a3e9e
Mikołaj Ewald <mikolajewald@gmail.com>
2020-02-13 21:47:47 +0100

    sidebar fixed and calendar table header

--- client/src/components/containers/Calendar.js
+++ client/src/components/containers/Calendar.js
@@ -7,7 +7,7 @@ const Calendar = () => {
     const [year, setYear] = useState(date.getFullYear());
     const [month, setMonth] = useState(date.getMonth() + 1);
     const [day, setDay] = useState(date.getDate());
-    
+
     useEffect(() => {
         const interval = setInterval(() => updateDate(), 1000);
 
@@ -15,6 +15,51 @@ const Calendar = () => {
             clearInterval(interval);
         }
     });
+
+    const week = [
+        {
+            dayNumber: 1, 
+            dayFullName: 'Poniedziałek',
+            dayShortName: 'Pn',
+        },
+        {
+            dayNumber: 2, 
+            dayFullName: 'Wtorek',
+            dayShortName: 'Wt',
+        },
+        {
+            dayNumber: 3, 
+            dayFullName: 'Środa',
+            dayShortName: 'Śr',
+        },
+        {
+            dayNumber: 4, 
+            dayFullName: 'Czwartek',
+            dayShortName: 'Czw',
+        },
+        {
+            dayNumber: 5, 
+            dayFullName: 'Piątek',
+            dayShortName: 'Pt',
+        },
+        {
+            dayNumber: 6, 
+            dayFullName: 'Sobota',
+            dayShortName: 'So',
+        },
+        {
+            dayNumber: 7, 
+            dayFullName: 'Niedziela',
+            dayShortName: 'Nd',
+        }
+    ];
+
+    const calendarHeader = [];
+
+    for (const [index, value] of week.entries()) {
+        calendarHeader.push(<th key={index}>{value.dayShortName}</th>);
+    }
+    
 
     function updateDate() {
         setDate(new Date());
@@ -27,7 +72,17 @@ const Calendar = () => {
         <div>
             <h2>{year}</h2>
             <h3>{month}</h3>
-            <p>{day}</p>
+            <table>
+                <thead>
+                    <tr>
+                        {calendarHeader}
+                    </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+
         </div>
     )
 }
--- client/src/components/containers/Navigation.js
+++ client/src/components/containers/Navigation.js
@@ -1,18 +1,32 @@
 import './../../App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navigation = () => {
 
     const [state, setState] = useState('home');
 
+    useEffect(() => {
+        switch (window.location.pathname) {
+            case '/home' : 
+                setState('home'); 
+                break;
+            case '/calendar' : 
+                setState('calendar'); 
+                break;
+            default : 
+                setState('home'); 
+                break;
+        }
+    }, []);
+
     return (
         <nav className='navbar'>
             <div onClick={() => setState('home')} className={state === 'home' ? 'navbar__element navbar__element--active' : 'navbar__element'}>
-            <Link to="/" className="navbar__element__link" >{state}</Link>
+            <Link to="/" className="navbar__element__link" ></Link>
             </div>
             <div onClick={() => setState('calendar')} className={state === 'calendar' ? 'navbar__element navbar__element--active' : 'navbar__element'}>
-                <Link to="/calendar" className="navbar__element__link" >{state}</Link>
+                <Link to="/calendar" className="navbar__element__link" ></Link>
             </div>
         </nav>
     );
