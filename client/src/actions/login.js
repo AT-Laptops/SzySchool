444d1fbaacb85dc7ed738c900a9b32cfd1bee9ce
Mikołaj Ewald <mikolajewald@gmail.com>
2020-02-17 22:47:59 +0100

    login layout done

---
+++ client/src/actions/login.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { LOGIN_SUCCESS, LOGIN_FAIL } from './types';
+
+export const login = (email, password) => async dispatch => {
+
+    const config = {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      };
+      const body = JSON.stringify({ email, password });
+      
+}
--- client/src/components/layouts/Login.js
+++ client/src/components/layouts/Login.js
@@ -1,19 +1,58 @@
 import './../../App.css';
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+
+const validate = form => {
+    if(!form.email) {
+        return 'Musisz podać email';
+    }
+
+    if (!form.password) {
+        return 'Wpisz hasło';
+    }
+
+    return null;
+}
 
 const Login = () => {
     const [error, setError] = useState(null);
     const [form, setForm] = useState({
-        login: '',
+        email: '',
         password: '',
     });
 
+    const { email, password } = form;
+
+    const updateField = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const errorMsg = validate(form);
+        if (errorMsg) {
+            setError(errorMsg);
+            return;
+        }
+        console.log(form);
+    }
+
     return (
-        <form action="">
-            <input type="text"/>
-            <input type="text"/>
+        <form className={ 'form' } action="" onSubmit={ handleSubmit } >
+            <p className={ 'form__error' }>{ error }</p>
+            <div className={ 'form__wrapper'}>
+                <label className={ 'form__wrapper__label' } htmlFor="email">Email</label>
+                <input type="email" className={ 'form__wrapper_input' } name='email' value={ email } onChange={ updateField } />
+            </div>
+            <div className={ 'form__wrapper'}>
+                <label className={ 'form__wrapper__label' } htmlFor="password">Hasło</label>
+                <input type="password" className={ 'form__wrapper_input' } name='password' value={ password } onChange={ updateField } />
+            </div>
+            <input type="submit" className={ 'form__submit' } value='Zarejestruj' onClick={ handleSubmit } />
         </form>
     )
 }
