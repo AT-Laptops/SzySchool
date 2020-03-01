4d832773e898f2693a2743f6b78d2b70ef0b8cee
Mikołaj Ewald <mikolajewald@gmail.com>
2020-03-01 23:57:11 +0100

    todos site update

--- client/src/components/containers/Todos.js
+++ client/src/components/containers/Todos.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, connect } from 'react-redux';
 import { setTodo } from './../../actions/setTodo';
 import { todos } from './../../actions/todos';
-import PropTypes from 'prop-types';
+import PropTypes, { func } from 'prop-types';
 
 const Todos = ({setTodo, todos}) => {
     const date = useSelector(state => state.reminder.date)
@@ -11,26 +11,47 @@ const Todos = ({setTodo, todos}) => {
     const day = useState(`${(date.getDate()).toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}r.`);
 
     useEffect(() => {
-        todos(date);  
+        todos(date);
     }, []);
 
-    const todosList = allTodos.map((todo) => 
-        <div className='todos__undone__wrapper'  key={ todo._id }>
-        <p className={ todo.isDone ? 'todos__undone__wrapper__todo todos__undone__wrapper__todo--done' : 'todos__undone__wrapper__todo'  }>
-            { todo.content }
-        </p>
-        <button onClick={ () => { setTodo(todo, date) } }   className='todos__undone__wrapper__button' >Lorem</button>
-        </div>
-    );
+    const undoneTodos = allTodos.map((todo) => {
+        if (todo.isDone) {
+            return (
+                <div className='todos__undone__wrapper'  key={ todo._id }>
+                    <div onClick={ () => { setTodo(todo, date) } }   className={ !todo.isDone ? 'todos__undone__wrapper__button todos__undone__wrapper__button--done' : 'todos__undone__wrapper__button' } ></div>
+                    <p className='todos__undone__wrapper__todo' >
+                        { todo.content }
+                    </p>
+                </div>
+            )
+        } 
+        return true;
+    });
 
+    const doneTodos = allTodos.map((todo) => {
+        if (!todo.isDone) {
+            return (
+                <div className='todos__undone__wrapper'  key={ todo._id }>
+                    <div onClick={ () => { setTodo(todo, date) } }   className={ !todo.isDone ? 'todos__undone__wrapper__button todos__undone__wrapper__button--done' : 'todos__undone__wrapper__button' } ></div>
+                    <p className='todos__undone__wrapper__todo'  >
+                        { todo.content }
+                    </p>
+                </div>
+            )
+        }
+        return true;
+    })
 
     return (
         <section className='todos'>
             <h2 className='todos__header'>{day}</h2>
             <div className='todos__undone'>
-                { todosList }
+                { undoneTodos }
             </div>
-            <div className='todos__done'></div>
+            <div className='todos__done'>
+                <h3>Ukończono:</h3>
+                { doneTodos }
+            </div>
         </section>
     )
 }
