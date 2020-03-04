4d3d18c383a27ebf3aa8fb7a15a17b516195ba75
Mikołaj Ewald <mikolajewald@gmail.com>
2020-03-04 22:04:56 +0100

    day component done

--- client/src/components/layouts/Home.js
+++ client/src/components/layouts/Home.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar';
-import Notes from './../containers/Notes';
+import UndoneTodos from './../presentation/UndoneTodos';
+import { useSelector } from 'react-redux';
 
 const Home = () => {
+    const date = new Date();
+    const day = useState(`${(date.getDate()).toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}r.`);
     return (
         <div className='home'>
-            <Notes></Notes>
+            <h3 home__header> { day } </h3>
+            <UndoneTodos date={ date }></UndoneTodos>
             <Sidebar></Sidebar>
         </div>
     )
