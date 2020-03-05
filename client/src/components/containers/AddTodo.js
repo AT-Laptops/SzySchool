5b8506226f8afe62daa103078f42881bcb9fe361
Mikołaj Ewald <mikolajewald@gmail.com>
2020-03-06 00:22:00 +0100

    working adding todos

--- client/src/components/containers/AddTodo.js
+++ client/src/components/containers/AddTodo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback} from 'react';
 import useInput from './../containers/useInput'
 import { useSelector, useDispatch } from 'react-redux';
 import { addTodo } from './../../actions/addTodo';
@@ -7,18 +7,22 @@ const AddTodo = () => {
     const day = useSelector(state => state.reminder.date);
     const dispatch = useDispatch();
     const header = useState(`${(day.getDate()).toString().padStart(2, '0')}.${(day.getMonth() + 1).toString().padStart(2, '0')}.${day.getFullYear()}r.`);
+    const [, updateComponent] = useState();
     const {form, handleChange } = useInput({
         date: day,
         content: '',
     });
+    const forceUpdate = useCallback(() => updateComponent({}), []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
         dispatch(addTodo(form.content, form.date));
+        form.content = '';
+        forceUpdate();
     }
 
     return (
-        <form onSubmit={ handleSubmit }>
+        <form onSubmit={ handleSubmit } className='addTodoForm'>
             <h2>{ header }</h2>
             <input type="text" name='content' value={ form.content } onChange={ handleChange }/>
             <input type='submit' value='Dodaj' onClick={ handleSubmit }/>
