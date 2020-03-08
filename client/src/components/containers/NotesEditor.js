5e4e12378d930d8967075dba7175a89cde874930
Mikołaj Ewald <mikolajewald@gmail.com>
2020-03-08 18:49:23 +0100

    gitignore

--- client/src/components/containers/NotesEditor.js
+++ client/src/components/containers/NotesEditor.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
+import * as data from './../../config.json';
 
 const NotesEditor = () => {
     return (
         <Editor
+            apiKey={ data.apiKey }
         >
             
         </Editor>
